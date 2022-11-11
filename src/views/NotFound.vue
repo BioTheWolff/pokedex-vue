15840d3533fce3f88cc76fd42b093e2bbc25b8d6
BioTheWolff <biothewolff@gmx.fr>
2022-11-11 14:31:11 +0100

    feat(404): adding a not found page in case the user gets lost

---
+++ src/views/NotFound.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+function back() {
+    window.history.go(-1)
+}
+
+function home(router) {
+    router.push({path: '/'})
+}
+</script>
+
+<template>
+    <p>Sorry, I think you got lost!</p>
+    <p>You should either <button @click="back()">go back</button> or <button @click="home($router)">go to the homepage</button></p>
+</template>
